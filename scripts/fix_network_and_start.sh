be326482729b2350f55a7322506e6ad11e9f596c
root <root@brain.thesteamedcrab.com>
2016-11-21 18:59:48 -0800

    Reduce startup delay

--- scripts/fix_network_and_start.sh
+++ scripts/fix_network_and_start.sh
@@ -9,6 +9,6 @@ ip addr flush dev ${IFNAME}
 ip route add 192.168.1.0/24 dev ${IFNAME} 
 ip route add default via 192.168.1.1
 
-sleep 10
+sleep 1
 
 /usr/local/bin/hass --config /config --log-rotate-days 7
