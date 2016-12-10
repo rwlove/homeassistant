4c3807d9a2c471040d79ee8210f323d3113bff2f
root <root@brain.thesteamedcrab.com>
2016-12-10 09:24:44 -0800

    Add interactive option to run.sh

--- run.sh
+++ run.sh
@@ -2,16 +2,31 @@
 
 . settings.conf
 
+MODE=d #default is daemon
+
+while getopts ":i" opt; do
+    case $opt in
+	i)
+	    MODE=i
+	    CMD=/bin/bash
+	    ;;
+	\?)
+	    echo "Invalid option: -$OPTARG" >&2
+	    ;;
+    esac
+done
+
+
 [ ! -d logs ] && mkdir logs/
 [ ! -f logs/home-assistant.log ] && touch logs/home-assistant.log
 
 docker run \
        --mac-address="${MAC_ADDRESS}" \
-       -dt \
+       -${MODE}t \
        --privileged \
        -h homeassistant \
        -v ${PWD}/logs/home-assistant.log:/config/home-assistant.log \
        -v ${PWD}/logs/OZW_Log.txt:/config/OZW_Log.txt \
        -v /etc/localtime:/etc/localtime:ro \
        -v /dev/ttyACM0:/dev/ttyACM0 \
-       services/homeassistant:latest
+       services/homeassistant:latest ${CMD}
