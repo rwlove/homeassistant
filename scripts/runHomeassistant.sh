810257a3045618bf8382787abf5f761f9c567bc9
root <root@brain.thesteamedcrab.com>
2017-06-11 17:14:57 -0700

    add hostname to wait loop output

--- scripts/runHomeassistant.sh
+++ scripts/runHomeassistant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while ! ping -c 1 brain; do
-    echo "Waiting for brian - network interface might be down..."
+    echo "(homeassistant) Waiting for brian - network interface might be down..."
     sleep 1
 done
 
