a597cd5be06a1ccb0af384068e14eb791bb78ddf
root <root@brain.thesteamedcrab.com>
2017-08-03 16:42:54 -0700

    create build.log

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-./helper_scripts/create_config_tarball.sh
+echo -n "New build: " | tee -a build.log
+date | tee -a build.log
 
-docker build $* -t services/homeassistant:v0.50.2 .
+./helper_scripts/create_config_tarball.sh | tee -a build.log
+
+docker build $* -t services/homeassistant:v0.50.2 . 2>&1 | tee -a build.log
 
 rm config_files.tar.gz
 
-docker run --rm services/homeassistant:latest hass --script check_config --config /config
+docker run --rm services/homeassistant:latest hass --script check_config --config /config 2>&1 | tee -a build.log
