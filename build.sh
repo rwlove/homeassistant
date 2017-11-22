aa5588122d084e584058b1f2f5a86bd122d88959
root <root@brain.thesteamedcrab.com>
2017-11-22 13:44:42 -0800

    do not use latest for test

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo -n "New build: " | tee -a build.log
+BUILD_VER="v0.58.1"
+
+echo -n "New build: ${BUILD_VER}" | tee -a build.log
 date | tee -a build.log
 
 ./helper_scripts/create_config_tarball.sh | tee -a build.log
 
-docker build $* -t services/homeassistant:v0.58.1 . 2>&1 | tee -a build.log
+docker build $* -t services/homeassistant:${BUILD_VER} . 2>&1 | tee -a build.log
 
 rm config_files.tar.gz
 
-docker run --rm services/homeassistant:latest hass --script check_config --config /config 2>&1 | tee -a build.log
+docker run --rm services/homeassistant:${BUILD_VER} hass --script check_config --config /config 2>&1 | tee -a build.log
