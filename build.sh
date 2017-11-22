9c6d80fb53ada2097679b06233ed77ac66d712cb
root <root@brain.thesteamedcrab.com>
2017-11-22 13:54:42 -0800

    fix build test bug

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ docker build $* -t services/homeassistant:${BUILD_VER} . 2>&1 | tee -a build.log
 
 rm config_files.tar.gz
 
-docker run --rm services/homeassistant:${BUILD_VER} hass --script check_config --config /config 2>&1 | tee -a build.log
+docker run --rm services/homeassistant:${BUILD_VER} python -m homeassistant --script check_config --config /config 2>&1 | tee -a build.log
