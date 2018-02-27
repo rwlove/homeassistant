e8c98cf16d46939c7778015a7fb821193f2f13c9
Your Name <you@example.com>
2018-02-26 18:27:19 -0800

    add VERSION to settings.conf.example

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-BUILD_VER="v0.64"
+. settings.conf
 
-echo -n "New build: ${BUILD_VER}" | tee -a build.log
+echo -n "New build: v${HASS_VERSION}" | tee -a build.log
 date | tee -a build.log
 
 ./helper_scripts/create_config_tarball.sh | tee -a build.log
 
-docker build $* -t services/homeassistant:${BUILD_VER} . 2>&1 | tee -a build.log
+docker build $* -t services/homeassistant:v${HASS_VERSION} . 2>&1 | tee -a build.log
 
 rm config_files.tar.gz
 
-docker run --rm services/homeassistant:${BUILD_VER} python -m homeassistant --script check_config --config /config 2>&1 | tee -a build.log
+docker run --rm services/homeassistant:v${HASS_VERSION} python -m homeassistant --script check_config --config /config 2>&1 | tee -a build.log
