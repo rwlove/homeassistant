df3c2c30713faab02f9d8df1d26ff380f14368fa
Your Name <you@example.com>
2020-04-16 14:30:21 -0700

    split custom components out

--- build.sh
+++ build.sh
@@ -6,11 +6,14 @@ echo -n "New build: v${HASS_VERSION}" | tee -a build.log
 date | tee -a build.log
 
 pushd config_files/custom_components/
+CURR_DIR=${PWD}
 
-git_url=`git config --get remote.origin.url`
+rm -rf sonoff
+mkdir /tmp/ewelink && pushd /tmp/ewelink
+git clone https://github.com/peterbuga/HASS-sonoff-ewelink
+mv HASS-sonoff-ewelink/sonoff ${CURR_DIR}/
+popd && rm -rf /tmp/ewelink
 
-echo -n "Pull ${git_url}"
-git pull
 popd
 
 ./helper_scripts/create_config_tarball.sh | tee -a build.log
